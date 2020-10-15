98c57213607d6b2c74a37fa8db8998f6d3289411
Jéssica Martins <jessmartins@poli.ufrj.br>
2020-10-15 13:28:15 -0300

    Atualiza dados passados pelo dto

---
+++ src/main/java/br/com/stickers/StickersApp/DTO/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package br.com.stickers.StickersApp.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioDTO {
+	
+	private String nome;
+	
+	private String celular;
+	
+	private String email;
+	
+	private String pais;
+
+}
