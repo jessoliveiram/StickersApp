c28e70bdf4635cf591a250e88b036311914ed7e8
Jéssica Martins <jessmartins@poli.ufrj.br>
2020-10-15 13:32:59 -0300

    Atualiza chamadas ao repositório pelo service, atualizando métodos em ambos

--- src/main/java/br/com/stickers/StickersApp/Repository/UsuarioRepository.java
+++ src/main/java/br/com/stickers/StickersApp/Repository/UsuarioRepository.java
@@ -7,5 +7,7 @@ import br.com.stickers.StickersApp.Model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+	
+	public Usuario findUsuarioById(Long id);
 
 }
--- src/main/java/br/com/stickers/StickersApp/Service/UsuarioService.java
+++ src/main/java/br/com/stickers/StickersApp/Service/UsuarioService.java
@@ -1,8 +1,13 @@
 package br.com.stickers.StickersApp.Service;
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.stickers.StickersApp.DTO.UsuarioDTO;
+import br.com.stickers.StickersApp.Model.Usuario;
 import br.com.stickers.StickersApp.Repository.UsuarioRepository;
 
 @Service
@@ -10,6 +15,30 @@ public class UsuarioService {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+
 	
+	public Usuario getUsuario(Long id) {
+		
+		Usuario usuario = usuarioRepository.findUsuarioById(id);
+		
+		return usuario;
+	}
+	
+	public Usuario updateUsuario(Long id, UsuarioDTO usuarioDTO) {
+		
+		ModelMapper mm = new ModelMapper();
+		Usuario usuario = mm.map(usuarioDTO, Usuario.class);
+		usuario.setId(id);
+		
+		return usuario;
+		
+	}
+	
+	public List<Usuario> getListUsuario() {
+		
+		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
+		
+		return usuarios;
+	}
 
 }
