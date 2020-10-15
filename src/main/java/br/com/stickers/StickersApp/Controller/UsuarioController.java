4994a9935ed75bf8c60f17fdfb59ac6a3b3fd2fa
Jéssica Martins <jessmartins@poli.ufrj.br>
2020-10-15 13:29:40 -0300

    Cria endpoints no controller para acessar usuario service

---
+++ src/main/java/br/com/stickers/StickersApp/Controller/UsuarioController.java
@@ -0,0 +1,49 @@
+package br.com.stickers.StickersApp.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.stickers.StickersApp.DTO.UsuarioDTO;
+import br.com.stickers.StickersApp.Model.Usuario;
+import br.com.stickers.StickersApp.Service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@GetMapping(path = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> getUsuario(@PathVariable Long id) {
+		
+		Usuario response = usuarioService.getUsuario(id);
+		
+		return ResponseEntity.ok().body(response);
+	}
+	
+	@PutMapping(path = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> updateUsuario(@PathVariable Long id, @RequestBody UsuarioDTO usuarioDTO) {
+		
+		Usuario response = usuarioService.updateUsuario(id, usuarioDTO);
+		
+		return ResponseEntity.ok().body(response);
+	}
+	
+	@GetMapping(produces = "application/json")
+	public ResponseEntity<List<Usuario>> getListUsuario() {
+		
+		List<Usuario> response = usuarioService.getListUsuario();
+		
+		return ResponseEntity.ok().body(response);
+	}
+
+}
